1fcc16cfbd3f9ee06ccbf4053ab9c80716632280
pavstar619 <52383535+pavstar619@users.noreply.github.com>
2020-05-21 17:09:44 +0600

    search engine

--- searchapp/backend-search.php
+++ searchapp/backend-search.php
@@ -1,13 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-try{
-    $pdo = new PDO("mysql:host=localhost;dbname=phplearning", "root", "");
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
-    die("ERROR: Could not connect. " . $e->getMessage());
-}
+include_once "connect.php";
  
 // Attempt search query execution
 try{
